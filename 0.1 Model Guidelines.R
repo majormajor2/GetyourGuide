582f34b379402a104728ea472a28ef3751189ce3
Md. Hamayun Azam Khan <khanmd.h.hub@rdc.wiwi.hu-berlin.de>
2017-05-21 18:26:29 +0200

    Guidelines

---
+++ 0.1 Model Guidelines.R
@@ -0,0 +1,8 @@
+
+#Our model should be able to:
+
+#1. Distinguish between bookings and non-bookings
+#2. Identify time-trends at the week, month, day level
+#3. Identify price elasticities
+#4. Rank Accounts, Device Type and Match Types by probability of order
+#5. Identify high revenue bookings
