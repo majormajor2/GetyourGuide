9e551cb82cb200702951f1dc6d2826d0c33ea0ac
Majormajor2 <jahaz707@gmail.com>
2017-05-22 09:29:15 +0200

    last final lol

--- 3.0 Main.R
+++ 3.0 Main.R
@@ -15,11 +15,11 @@ train = read.csv("train.csv")
 
 #Data Setup
 train = data_setup(train)
-prediction = data_setup(prediction)
+#prediction = data_setup(prediction)
 
 #Time Features
 train = time_features(train)
-prediction = time_features(prediction)
+#prediction = time_features(prediction)
 
 #Data Exploration
 par(mar=c(1,1,1,1)) # to make sure the plot works on a small screen
@@ -28,50 +28,52 @@ par(mar=c(1,1,1,1)) # to make sure the plot works on a small screen
 
 
 #Apply WOE to factors with high number of levels
-train = calculate_woe(train)
-train = apply_woe(train)
+#train = calculate_woe(train)
+#train = apply_woe(train)
+
+#file is too big for my computer
 
 #check if there are new levels in Prediction and set them to 0
-train_factors = train[,c(2:7)]
-prediction.new.levels = check_new_levels(train = train_factors, prediction = prediction)
+#train_factors = train[,c(2:7)]
+#prediction.new.levels = check_new_levels(train = train_factors, prediction = prediction)
 
 #26189 new levels in Keywords
 #17995 new levels in Ad_Groups
 #33 new levels in Campaigns
 
-key_missing = as.vector(prediction.new.levels$Keyword_ID)
-ad_missig = as.vector(prediction.new.levels$Ad_group_ID)
-campaign_missng = as.vector(prediction.new.levels$Campaign_ID)
+#key_missing = as.vector(prediction.new.levels$Keyword_ID)
+#ad_missig = as.vector(prediction.new.levels$Ad_group_ID)
+#campaign_missng = as.vector(prediction.new.levels$Campaign_ID)
 
 #index new levels
-new_levels_index = which(prediction$Keyword_ID %in% key_missing 
-                         | prediction$Ad_group_ID %in% ad_missig 
-                         | prediction$Campaign_ID %in% campaign_missng)
+#new_levels_index = which(prediction$Keyword_ID %in% key_missing 
+                         #| prediction$Ad_group_ID %in% ad_missig 
+                         #| prediction$Campaign_ID %in% campaign_missng)
 
 #set new levels to zero
-prediction.levels.zero = prediction[new_levels_index,]
-prediction.levels.zero$Keyword_ID = 0
-prediction.levels.zero$Ad_group_ID = 0
-prediction.levels.zero$Campaign_ID = 0
+#prediction.levels.zero = prediction[new_levels_index,]
+#prediction.levels.zero$Keyword_ID = 0
+#prediction.levels.zero$Ad_group_ID = 0
+#prediction.levels.zero$Campaign_ID = 0
 
 #remove indexed rows from prediction
-prediction.no.new.levels = prediction[-new_levels_index,]
+#prediction.no.new.levels = prediction[-new_levels_index,]
 #remove new factor levels
-prediction.no.new.levels = remove_new_levels(prediction.no.new.levels)
+#prediction.no.new.levels = remove_new_levels(prediction.no.new.levels)
 #check new levels
-check_new_levels(train = train_factors, prediction = prediction.no.new.levels) #no new levels
+#check_new_levels(train = train_factors, prediction = prediction.no.new.levels) #no new levels
 
 
 #merge the two dfs to create a new prediction dataframe
-prediction_woe = rbind(prediction.levels.zero, prediction.no.new.levels)
-saveRDS(object = prediction_woe, file = "prediction_woe.rds")
+#prediction_woe = rbind(prediction.levels.zero, prediction.no.new.levels)
+#saveRDS(object = prediction_woe, file = "prediction_woe.rds")
 
 
 #Apply WOE to factors with high number of levels i.e. Keyword, Ad Group and Campaign
-woe_columns = c("Keyword_ID","Ad_group_ID","Campaign_ID", "booking")
-train_woe = train_subset[,woe_columns]
-train = calculate_woe(train = train_woe)
-prediction = apply_woe(prediction_woe)
+#woe_columns = c("Keyword_ID","Ad_group_ID","Campaign_ID", "booking")
+#train_woe = train_subset[,woe_columns]
+#train = calculate_woe(train = train_woe)
+#prediction = apply_woe(prediction_woe)
 
 #Predicted Features
 #revenue
@@ -105,13 +107,13 @@ stopCluster(cl)
 #Create feature for testset and Prediction
 
 testset$Revenue_feature = predict(ols, newdata = testset)
-prediction$Revenue_feature = predict(ols, newdata = prediction)
+#prediction$Revenue_feature = predict(ols, newdata = prediction)
 
 
 #K-Nearest Neighbors
 testset_dist = testset[,-c(17:19)]
 
-prediction$dist = get_dist(trainSet = testset, predSet = prediction, k = 5)
+#prediction$dist = get_dist(trainSet = testset, predSet = prediction, k = 5)
 testset$dist = get_dist(trainSet = testset_dist, predSet = testset_dist, k = 5)
 
 #Feature Importance
